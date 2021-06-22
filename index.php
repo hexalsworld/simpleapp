bd5df27c263efbc6586b5848c9207efc223eacf2
hexal <r4nt0tthus@gmail.com>
2021-06-22 22:21:22 +0200

    updated header

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>PHP Test</h1>
-		<p><b>An Example of PHP in Action</b></p>
+		<p><b>Welcome from Pusztulatka, he failed this build 2 times already</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
